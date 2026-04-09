d93789f1531d555381b61d3545cc42807877735b
Mamii868 <marcmotenii@gmail.com>
2026-04-09 15:15:39 -0500

    Add package and initial question

---
+++ src/main/java/com/pluralsight/Calculator.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+
+    }
+}
